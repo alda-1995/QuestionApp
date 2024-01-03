d0a9e774e2d7d3f94f57bef13d06eebd9d56f6cd
alda-1995 <aldair@canalla.agency>
2024-01-03 17:11:16 -0600

    se agrega index test

--- resources/views/components/general/sidenav.blade.php
+++ resources/views/components/general/sidenav.blade.php
@@ -10,9 +10,20 @@
             <p class="text-gray-400 small mb-2">General</p>
         </li>
         <li>
-            <x-general.menu-item label="Tests" url="dashboard">
+            <x-general.menu-item label="Dashboard" url="dashboard">
                 <x-slot name="icon">
                     <svg xmlns="http://www.w3.org/2000/svg" class="fill-gray-500" height="20px" width="20px" viewBox="0 0 24 24">
+                        <title>view-dashboard</title>
+                        <path d="M13,3V9H21V3M13,21H21V11H13M3,21H11V15H3M3,13H11V3H3V13Z" />
+                    </svg>
+                </x-slot>
+            </x-general.menu-item>
+        </li>
+        <li>
+            <x-general.menu-item label="Tests" url="test.index">
+                <x-slot name="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="fill-gray-500" height="20px" width="20px"
+                        viewBox="0 0 24 24">
                         <title>help</title>
                         <path
                             d="M10,19H13V22H10V19M12,2C17.35,2.22 19.68,7.62 16.5,11.67C15.67,12.67 14.33,13.33 13.67,14.17C13,15 13,16 13,17H10C10,15.33 10,13.92 10.67,12.92C11.33,11.92 12.67,11.33 13.5,10.67C15.92,8.43 15.32,5.26 12,5A3,3 0 0,0 9,8H6A6,6 0 0,1 12,2Z" />
@@ -25,9 +36,8 @@
                 onclick="event.preventDefault();
     document.getElementById('logout-form').submit();">
                 <div class="flex mr-2">
-                    <svg class="stroke-gray-500"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor">
+                    <svg class="stroke-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke-width="1.5" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                     </svg>
--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.dash-side')
 @section('content')
+<div class="py-16">
     <div class="container">
-        <h1 class="font-secondary text-neutral mb-6">Lista de test</h1>
+        <h1 class="font-secondary text-neutral mb-6">Mis test</h1>
+        <div class="max-w-[260px] mb-6">
+            <form action="">
+                <x-controls.search />
+            </form>
+        </div>
+        <div class="flex flex-col max-w-2xl">
+            <x-cards.item-test name="Test de personalidad" status="completado" />
+            <x-cards.item-test name="Test de personalidad" status="completado" />
+            <x-cards.item-test name="Test de personalidad" status="completado" />
+            <x-cards.item-test name="Test de personalidad" status="completado" />
+        </div>
     </div>
+</div>
 @endsection
