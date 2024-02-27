5213cad0c9319b3cfed4ddf3cbc1d231d04d0457
alda-1995 <aldair@canalla.agency>
2024-02-26 18:16:04 -0600

    se agrega redireccion despues de responder una pregunta

--- app/Http/Controllers/AnswerTestController.php
+++ app/Http/Controllers/AnswerTestController.php
@@ -12,26 +12,6 @@ use Illuminate\Support\Facades\Auth;
 class AnswerTestController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -57,50 +37,6 @@ class AnswerTestController extends Controller
            'test_result_id' => $idTestResult,
            'question_id' => $idQuestion,
         ]);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return redirect()->route('player.test.show', ['idPlayerTest' => $idTestResult]);
     }
 }
